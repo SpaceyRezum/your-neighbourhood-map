cbb9274f2e8e4d83470467c3831a9e04de49c7c9
SpaceyRezum <info@alexis-bellet.com>
2016-11-10 20:24:15 +0100

    binded DOM and address input through Knockout observable

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ through the knockout framework is stored */
 var ViewModel = {
 
 	map: {}, // Created map as a global variable to be able to access and modify it later on
+	address: ko.observable(''),
 	formattedAddress: ko.observable(''), // Observable value to display the formatted address of each research
 	errorStatus: 'Sorry, the address couldn\'t be found due to ', // Stores a standard error status
 
@@ -18,15 +19,15 @@ var ViewModel = {
 
 	// Function updating the map when user enters its input (address)
 	updateMap: function() {
-		var address = $('#address').val();
 		// creates a geocoder to retrieve lat/lng of string-type address
 		var geocoder = new google.maps.Geocoder();
 		// obtains the geocode of the user input
-		geocoder.geocode({address: address}, function(results, status) {
+		geocoder.geocode({address: ViewModel.address()}, function(results, status) {
 			if (status === google.maps.GeocoderStatus.OK) {
 				if (results[0]) {
 					// centers the map with geocode results
 			  	map.setCenter(results[0].geometry.location);
+			  	map.setZoom(10);
 			  	// Updates the formattedAddress binding
 			  	ViewModel.formattedAddress(results[0].formatted_address);
 				};
